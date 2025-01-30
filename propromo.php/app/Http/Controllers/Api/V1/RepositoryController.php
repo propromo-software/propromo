99751c49b400533e0d21401c909f8da98f609775
stevan06v <stevanvlajic5@gmail.com>
2025-01-30 03:02:55 +0000

    [propromo.php] feat: added repository...
    Mirrored from https://github.com/propromo-software/propromo.php/commit/99751c44b432a91839c9df4f15943bfc23d6ddac

--- propromo.php/app/Http/Controllers/Api/V1/RepositoryController.php
+++ propromo.php/app/Http/Controllers/Api/V1/RepositoryController.php
@@ -4,40 +4,54 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Monitor;
+use Illuminate\Http\JsonResponse;
 
 class RepositoryController extends Controller
 {
-    public function get_repository_by_monitor_hash($monitor_hash)
+    /**
+     * Get repositories by monitor hash.
+     *
+     * @param string $monitor_hash
+     * @return JsonResponse
+     */
+    public function getRepositoryByMonitorHash(string $monitor_hash): JsonResponse
     {
-        $monitor = Monitor::whereMonitorHash($monitor_hash)->first();
-        if( $monitor ){
-            $repositories = $monitor->repositories()->get();
-            return response()->json([
-                'success' => true,
-                'repositories' => $repositories
-            ], 200);
-        }else{
-            return response()->json([
-                'success' => true,
-                'repositories' => []
-            ], 200);
-        }
+        return $this->getRepositoriesByMonitor('monitor_hash', $monitor_hash);
     }
 
-    public function get_repository_by_monitor_id($id)
+    /**
+     * Get repositories by monitor ID.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function getRepositoryByMonitorId(int $id): JsonResponse
     {
-        $monitor = Monitor::whereId($id)->first();
-        if( $monitor ){
-            $repositories = $monitor->repositories()->get();
+        return $this->getRepositoriesByMonitor('id', $id);
+    }
+
+    /**
+     * Fetch repositories by monitor attribute.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return JsonResponse
+     */
+    private function getRepositoriesByMonitor(string $attribute, mixed $value): JsonResponse
+    {
+        $monitor = Monitor::where($attribute, $value)->first();
+
+        if (!$monitor) {
             return response()->json([
-                'success' => true,
-                'repositories' => $repositories
-            ], 200);
-        }else{
-            return response()->json([
-                'success' => true,
+                'success' => false,
+                'message' => 'Monitor not found.',
                 'repositories' => []
-            ], 200);
+            ], 404);
         }
+
+        return response()->json([
+            'success' => true,
+            'repositories' => $monitor->repositories()->get()
+        ], 200);
     }
 }

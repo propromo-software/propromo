5f2012001c5e086d3eb63de3db9a0934b2448065
stevan06v <stevanvlajic5@gmail.com>
2025-01-30 07:04:21 +0000

    [propromo.php] feat: store user request fixed
    Mirrored from https://github.com/propromo-software/propromo.php/commit/5f20120cc14b6f84539732ea6631c889fb0847bf

--- propromo.php/app/Http/Requests/V1/StoreUserRequest.php
+++ propromo.php/app/Http/Requests/V1/StoreUserRequest.php
@@ -22,17 +22,46 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required'],
-            'email' => ['required', 'email'],
-            'authType' => ['required'],
-            'password' => ['required']
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'auth_type' => ['required', 'string', 'in:google,github,email'],
+            'password' => [
+                'required',
+                'string',
+                'min:8', // At least 8 characters
+                'regex:/[A-Z]/', // At least 1 uppercase letter
+                'regex:/[a-z]/', // At least 1 lowercase letter
+                'regex:/[0-9]/', // At least 1 number
+                'regex:/[@$!%*?&#]/' // At least 1 special character
+            ]
         ];
     }
 
+    /**
+     * Automatically convert request attributes before validation.
+     */
     protected function prepareForValidation()
     {
         $this->merge([
-           'auth_type' => $this->authType
+            'auth_type' => $this->authType, // Convert camelCase to snake_case
         ]);
+    }
+
+    /**
+     * Custom validation messages.
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Name is required.',
+            'email.required' => 'Email is required.',
+            'email.email' => 'Provide a valid email address.',
+            'email.unique' => 'This email is already registered.',
+            'auth_type.required' => 'Auth type is required.',
+            'auth_type.in' => 'Auth type must be one of: google, github, email.',
+            'password.required' => 'Password is required.',
+            'password.min' => 'Password must be at least 8 characters long.',
+            'password.regex' => 'Password must contain at least 1 uppercase letter, 1 number, and 1 special character.'
+        ];
     }
 }

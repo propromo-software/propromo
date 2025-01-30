ba395b6321751a9164568bfa29e73a4df4ab192c
stevan06v <stevanvlajic5@gmail.com>
2025-01-30 07:06:03 +0000

    [propromo.php] feat: user-collections improved
    Mirrored from https://github.com/propromo-software/propromo.php/commit/ba395b6c56a3e484eb0afc14fac172e827727e9a

--- propromo.php/app/Http/Resources/V1/UserCollection.php
+++ propromo.php/app/Http/Resources/V1/UserCollection.php
@@ -10,10 +10,30 @@ class UserCollection extends ResourceCollection
     /**
      * Transform the resource collection into an array.
      *
-     * @return array<int|string, mixed>
+     * @return array<string, mixed>
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'success' => true,
+            'total_users' => $this->collection->count(),
+            'users' => $this->collection->map(fn($user) => new UserResource($user)), // Wrap with UserResource
+        ];
+    }
+
+    /**
+     * Add meta information to the response.
+     *
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function with(Request $request): array
+    {
+        return [
+            'meta' => [
+                'api_version' => '1.0',
+                'request_time' => now()->toDateTimeString(),
+            ]
+        ];
     }
 }

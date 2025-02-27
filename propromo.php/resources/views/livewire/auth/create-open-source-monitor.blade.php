66ba4bf731fa37d887dfcd20f32e20c9d9d7ba0d
Jonas <publik.froeller.jonas@gmx.at>
2025-02-27 00:14:33 +0000

    [propromo.php] improves styling of `/create-monitor` and `/join` (closes propromo-software/propromo.php#340)
    Mirrored from https://github.com/propromo-software/propromo.php/commit/66ba4bf7aa65a549d684cd94096a4ee0f9570ef5

--- propromo.php/resources/views/livewire/auth/create-open-source-monitor.blade.php
+++ propromo.php/resources/views/livewire/auth/create-open-source-monitor.blade.php
@@ -11,7 +11,7 @@ new class extends Component
     public $error_head;
     public $project_url;
     public $pat_token = "use-open-source-program";
-    public $disable_pat_token = true; // New property to manage the checkbox state
+    public $disable_pat_token = true;
 
     protected $rules = [
         'project_url' => 'required|min:10|max:2048'
@@ -32,52 +32,55 @@ new class extends Component
             return redirect('/register');
         }
     }
+
     function switchTo()
     {
         return $this->redirect('create-monitor');
     }
 }; ?>
 
+<div class="flex flex-col items-center w-full h-full bg-gray-100">
+    <div class="flex flex-col justify-center mt-10">
+        <div class="flex gap-1 items-center mt-16 mb-2 sm:mt-10">
+            <div class="w-[30px] h-[30px] rounded-full bg-primary-blue"></div>
+            <div class="w-2 h-1 bg-primary-blue"></div>
+            <div class="w-[30px] h-[30px] bg-primary-blue rounded-full"></div>
+        </div>
 
-<div class="flex flex-col items-center mt-4 bg-gray-100 sm:justify-center sm:pt-0 dark:bg-gray-900">
-    <div class="w-full sm:max-w-md mt-6 p-12 bg-white dark:bg-gray-800 border-[1px] border-border-color sm:rounded-lg">
+        <div class="px-10 pt-8 pb-8 mx-auto w-96 max-w-full bg-white rounded-lg border border-border-color">
+            <h1 class="mb-8 text-6xl uppercase font-koulen text-primary-blue">Create Monitor</h1>
 
-        <div class="flex justify-center">
-            <div class="w-full max-w-md">
-                <h1 class="mb-9 text-6xl font-koulen text-primary-blue">CREATE MONITOR</h1>
-                <form wire:submit.prevent="create">
-                    <sl-input required wire:model="project_url" placeholder="Your Project-URL" type="text"></sl-input>
-                    <br>
-                    <sl-switch checked wire:click="switchTo()">Open Source</sl-switch>
+            <form wire:submit.prevent="create" class="flex flex-col gap-2">
+                <sl-input size="medium" required wire:model="project_url" placeholder="Your Project URL" type="text"></sl-input>
+                <sl-switch class="pt-1 text-secondary-grey" size="medium" wire:click="switchTo()" checked>Open Source</sl-switch>
 
-                    <!-- <div class="relative mt-2 w-full aspect-video">
-                        <iframe 
-                            class="absolute top-0 left-0 w-full h-full rounded-lg"
-                            src="https://player.vimeo.com/video/953693369?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&background=1&responsive=1" 
-                            frameborder="0" 
-                            allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
-                            title="Propromo Preview">
-                        </iframe>
-                    </div> -->
+                <div class="flex justify-between items-end mt-5">
+                    <a class="text-sm no-underline text-primary-blue hover:underline"
+                        href="{{ url('join') }}">
+                        Already have a monitor?
+                    </a>
 
-                    <div class="flex justify-between items-center mt-5">
-                        <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ url('join') }}">
-                            Already existing monitor?
-                        </a>
-
-                        <sl-button wire:loading.attr="disabled" type="submit">Create</sl-button>
-                    </div>
-                </form>
-            </div>
+                    <sl-button wire:loading.attr="disabled" type="submit">Create</sl-button>
+                </div>
+            </form>
         </div>
     </div>
 
     @if($create_monitor_error)
-        <sl-alert variant="danger" open closable>
-            <sl-icon wire:ignore slot="icon" name="patch-exclamation"></sl-icon>
-            <strong>{{$error_head}}</strong><br />
-            {{$create_monitor_error}}
-        </sl-alert>
+    <sl-alert variant="danger" open closable>
+        <sl-icon wire:ignore slot="icon" name="patch-exclamation"></sl-icon>
+        <strong>{{$error_head}}</strong><br />
+        {{$create_monitor_error}}
+    </sl-alert>
     @endif
-
 </div>
+
+<!-- <div class="relative mt-2 w-full aspect-video">
+    <iframe 
+        class="absolute top-0 left-0 w-full h-full rounded-lg"
+        src="https://player.vimeo.com/video/953693369?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&background=1&responsive=1" 
+        frameborder="0" 
+        allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
+        title="Propromo Preview">
+    </iframe>
+</div> -->
--- propromo.php/resources/views/livewire/auth/join-monitor.blade.php
+++ propromo.php/resources/views/livewire/auth/join-monitor.blade.php
@@ -28,42 +28,31 @@ new class extends Component
     }
 }; ?>
 
+<div class="flex flex-col items-center w-full h-full bg-gray-100">
+    <div class="flex flex-col justify-center mt-10">
+        <div class="flex gap-1 items-center mt-16 mb-2 sm:mt-10">
+            <div class="w-[30px] h-[30px] rounded-full bg-primary-blue"></div>
+            <div class="w-2 h-1 bg-primary-blue"></div>
+            <div class="w-[30px] h-[30px] bg-primary-blue rounded-full"></div>
+        </div>
 
-<div class="flex flex-col items-center mt-4 bg-gray-100 sm:justify-center sm:pt-0 dark:bg-gray-900">
-    <div class="w-full sm:max-w-md mt-6 p-12 bg-white dark:bg-gray-800 border-[1px] border-border-color sm:rounded-lg">
+        <div class="px-10 pt-8 pb-8 mx-auto w-96 max-w-full bg-white rounded-lg border border-border-color">
+            <h1 class="mb-8 text-6xl uppercase font-koulen text-primary-blue">Join Monitor</h1>
 
-        <div class="flex justify-center">
-            <div class="w-full max-w-md">
-                <h1 class="text-6xl font-koulen text-primary-blue mb-9">JOIN MONITOR</h1>
+            <form wire:submit="join" class="flex flex-col gap-2">
+                <sl-input size="medium" required wire:ignore wire:model="monitor_hash" placeholder="Project-Id" type="text"></sl-input>
 
-                <form wire:submit="join">
+                <div class="flex justify-between items-end mt-5">
+                    <a class="text-sm no-underline text-primary-blue hover:underline"
+                        href="{{ url('create-monitor') }}">
+                        No monitor yet?
+                    </a>
 
-                    <sl-input wire:ignore wire:model="monitor_hash" placeholder="Project-Id" type="text"></sl-input>
-
-                    <br>
-
-                    <!-- <div class="relative w-full mt-2 aspect-video">
-                        <iframe 
-                            class="absolute top-0 left-0 w-full h-full rounded-lg"
-                            src="https://player.vimeo.com/video/953693432?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&background=1&responsive=1" 
-                            frameborder="0" 
-                            allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
-                            title="Propromo Preview">
-                        </iframe>
-                    </div> -->
-
-                    <div class="flex items-center justify-between mt-5">
-                        <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ url('create-monitor') }}">
-                            No monitor yet?
-                        </a>
-
-                        <sl-button wire:ignore wire:loading.attr="disabled" type="submit">JOIN</sl-button>
-                    </div>
-                </form>
-            </div>
+                    <sl-button size="medium" wire:ignore wire:loading.attr="disabled" type="submit">Join</sl-button>
+                </div>
+            </form>
         </div>
     </div>
-
 
     @if($join_monitor_error)
     <sl-alert variant="danger" open closable>
@@ -72,5 +61,14 @@ new class extends Component
         {{$join_monitor_error}}
     </sl-alert>
     @endif
-
 </div>
+
+<!-- <div class="relative mt-2 w-full aspect-video">
+    <iframe 
+        class="absolute top-0 left-0 w-full h-full rounded-lg"
+        src="https://player.vimeo.com/video/953693432?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&background=1&responsive=1" 
+        frameborder="0" 
+        allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
+        title="Propromo Preview">
+    </iframe>
+</div> -->
